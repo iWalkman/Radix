8989b5d94ee27c6179af0f6bb8bcb38aa44de6f1
AppleWalkman <k.fomin98@gmail.com>
2014-02-11 15:23:56 +0400

    хз

--- Radix/RadixViewController.m
+++ Radix/RadixViewController.m
@@ -17,6 +17,8 @@
 
 - (IBAction)InputNumber {
     
+   // Number_textField.keyboardType = UIKeyboardTypeNumberPad;
+    
     NSString *Number = Number_textField.text;
     Num=[Number intValue];
     NSLog(@"Num = %d",Num);
@@ -102,6 +104,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     system_array = [[NSArray alloc] initWithObjects:@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",nil];
+
 }
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
